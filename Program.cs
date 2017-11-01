a35a30170db51fae87e08fa3e3e6d79920206c0c
Ronan Boiteau <ronan.boiteau@epitech.eu>
2017-11-01 16:46:36 +0100

    .NET Hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace coinche
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
