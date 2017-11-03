431c37ee48ac739fd12bad3b2fdd7655d034ee21
Ronan Boiteau <ronan.boiteau@epitech.eu>
2017-11-03 09:10:21 +0100

    Multi-client server

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,65 +1,19 @@
-﻿using System;  
-using System.Net;  
-using System.Text;  
-using System.Net.Sockets;  
-  
-namespace Server
- {
-    class TcpHelper
-       {  
-        private static TcpListener listener { get; set; }  
-        private static bool accept { get; set; } = false;  
-   
-        public static void StartServer(int port) {  
-            IPAddress address = IPAddress.Parse("127.0.0.1");  
-            listener = new TcpListener(address, port);  
-   
-            listener.Start();  
-            accept = true;  
-   
-            Console.WriteLine($"Server started. Listening to TCP clients at 127.0.0.1:{port}");  
-        }  
-   
-        public static void Listen()
-       {  
-            if(listener != null && accept) 
-            {  
-   
-                // Continue listening.  
-                while (true)
-                 {  
-                    Console.WriteLine("Waiting for client...");  
-                    var clientTask = listener.AcceptTcpClientAsync(); // Get the client  
-   
-                    if(clientTask.Result != null)
-                      {  
-                        Console.WriteLine("Client connected. Waiting for data.");  
-                        var client = clientTask.Result;  
-                        string message = "";  
-   
-                        while (message != null && !message.StartsWith("quit"))
-                         {  
-                            byte[] data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");  
-                            client.GetStream().Write(data, 0, data.Length);  
-   
-                            byte[] buffer = new byte[1024];  
-                            client.GetStream().Read(buffer, 0, buffer.Length);  
-   
-                            message = Encoding.ASCII.GetString(buffer);  
-                            Console.WriteLine(message);  
-                        }  
-                        Console.WriteLine("Closing connection.");  
-                        client.GetStream().Dispose();  
-                    }  
-                }  
-            }  
-        }  
-           public static void Main(string[] args)  
-           {  
-               // Start the server  
-               TcpHelper.StartServer(5678);  
-               TcpHelper.Listen(); // Start listening.  
-           }  
+﻿using System;
 
+namespace TCPServer.App.AfzaalAhmadZeeshan
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter the IP address where to run the TCP server: ");
+            var ip = Console.ReadLine();
+            Console.Write("Enter the port where to run the TCP server: ");
+            int port = Convert.ToInt32(Console.ReadLine());
+
+            // Start the server.
+            TcpServer.StartServer(port, ip);
+            TcpServer.Listen();
+        }
     }
-}  
+}
---
+++ Server/TcpServer.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace TCPServer.App.AfzaalAhmadZeeshan
+{
+    class TcpServer
+    {
+        private static TcpListener listener { get; set; }
+        private static bool accept { get; set; } = false;
+        private static int connectedClients { get; set; } = 0;
+
+        public static void StartServer(int port, string IP)
+        {
+            IPAddress address = IPAddress.Parse(IP);
+            listener = new TcpListener(address, port);
+
+            listener.Start();
+            accept = true;
+
+            Console.WriteLine($"Server started. Listening to TCP clients at {IP}:{port}");
+        }
+
+        public static void Listen()
+        {
+            if (listener != null && accept)
+            {
+                // Continue listening.
+                while (true)
+                {
+                    Console.WriteLine($"Waiting for client... {connectedClients} connected at the moment.");
+                    _listen(); // Helper to listen on the background and return.
+                }
+            }
+        }
+
+        private async static void _listen() {
+            var clientTask = listener.AcceptTcpClientAsync(); // Get the client
+
+            if (clientTask.Result != null)
+            {
+                connectedClients++;
+                Console.WriteLine("Client connected. Waiting for data.");
+                var client = clientTask.Result;
+                string message = "";
+
+                await Task.Run(() => {
+                    while (message != null && !message.StartsWith("quit"))
+                    {
+                        byte[] data = Encoding.ASCII.GetBytes("Send next data: [enter 'quit' to terminate] ");
+                        client.GetStream().Write(data, 0, data.Length);
+
+                        byte[] buffer = new byte[1024];
+                        client.GetStream().Read(buffer, 0, buffer.Length);
+
+                        message = Encoding.ASCII.GetString(buffer);
+                        Console.WriteLine(message);
+                    }    
+                });
+                
+                Console.WriteLine("Closing connection. {connectedClients} connected at the moment.");
+                client.GetStream().Dispose();
+                connectedClients--;
+            }
+        }
+    }
+}
