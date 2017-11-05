90db4e27c08dae548e677108b3735f771513bbcb
Fanny Tavart <fanny.tavart@epitech.eu>
2017-11-05 20:51:18 +0100

    beginning classes of the game

---
+++ Server/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Server
+{
+    public class Card
+    {
+        private String name;
+        private Suit suit;
+        private int id;
+
+        public Card(String name, Suit suit, int id)
+        {
+            this.name = name;
+            this.suit = suit;
+            this.id = id;
+        }
+
+        public String GetName()
+        {
+            return (name);
+        }
+
+        public Suit GetSuit()
+        {
+            return (suit);
+        }
+
+        public int GetId()
+        {
+            return (id);
+        }
+    }
+}
