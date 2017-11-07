3c5eb1c09b77b43c341c5de6f7e943f41e81cf75
Fanny Tavart <fanny.tavart@epitech.eu>
2017-11-07 11:03:10 +0100

    fix public methods

--- Server/Game.cs
+++ Server/Game.cs
@@ -5,61 +5,61 @@ namespace Server
 {
     public class Game
     {        
-        private Team[] teams = new Team[2];
-        private Deck modelDeck = new Deck(32);
+        private Team[] _teams = new Team[2];
+        private Deck _modelDeck = new Deck(32);
         private Deck _deck;
         private List<Player> _allPlayers;
         private Suit _trump;
-        private Trick trick = new Trick();
+        private Trick _trick = new Trick();
         
         public Game(List<Player> allPlayers, Team teamOne, Team teamTwo)
         {
-            teams[0] = teamOne;
-            teams[1] = teamTwo;
+            _teams[0] = teamOne;
+            _teams[1] = teamTwo;
             _allPlayers = allPlayers;
         }
 
         private void CreateModelDeck()
         {
-            modelDeck.AddCard(new Card("7", Suit.DIAMONDS, 0, 0));
-            modelDeck.AddCard(new Card("8", Suit.DIAMONDS, 0, 1));
-            modelDeck.AddCard(new Card("9", Suit.DIAMONDS, 0, 2));
-            modelDeck.AddCard(new Card("10", Suit.DIAMONDS, 10, 3));
-            modelDeck.AddCard(new Card("J", Suit.DIAMONDS, 2, 4));
-            modelDeck.AddCard(new Card("Q", Suit.DIAMONDS, 3, 5));
-            modelDeck.AddCard(new Card("K", Suit.DIAMONDS, 4, 6));
-            modelDeck.AddCard(new Card("A", Suit.DIAMONDS, 11, 7));
-            modelDeck.AddCard(new Card("7", Suit.CLUBS, 0, 8));
-            modelDeck.AddCard(new Card("8", Suit.CLUBS, 0, 9));
-            modelDeck.AddCard(new Card("9", Suit.CLUBS, 0, 10));
-            modelDeck.AddCard(new Card("10", Suit.CLUBS, 10, 11));
-            modelDeck.AddCard(new Card("J", Suit.CLUBS, 2, 12));
-            modelDeck.AddCard(new Card("Q", Suit.CLUBS, 3, 13));
-            modelDeck.AddCard(new Card("K", Suit.CLUBS, 4, 14));
-            modelDeck.AddCard(new Card("A", Suit.CLUBS, 11, 15));
-            modelDeck.AddCard(new Card("7", Suit.HEARTS, 0, 16));
-            modelDeck.AddCard(new Card("8", Suit.HEARTS, 0, 17));
-            modelDeck.AddCard(new Card("9", Suit.HEARTS, 0, 18));
-            modelDeck.AddCard(new Card("10", Suit.HEARTS, 10, 19));
-            modelDeck.AddCard(new Card("J", Suit.HEARTS, 2, 20));
-            modelDeck.AddCard(new Card("Q", Suit.HEARTS, 3, 21));
-            modelDeck.AddCard(new Card("K", Suit.HEARTS, 4, 22));
-            modelDeck.AddCard(new Card("A", Suit.HEARTS, 11, 23));
-            modelDeck.AddCard(new Card("7", Suit.SPADES, 0, 24));
-            modelDeck.AddCard(new Card("8", Suit.SPADES, 0, 25));
-            modelDeck.AddCard(new Card("9", Suit.SPADES, 0, 26));
-            modelDeck.AddCard(new Card("10", Suit.SPADES, 10, 27));
-            modelDeck.AddCard(new Card("J", Suit.SPADES, 2, 28));
-            modelDeck.AddCard(new Card("Q", Suit.SPADES, 3, 29));
-            modelDeck.AddCard(new Card("K", Suit.SPADES, 4, 30));
-            modelDeck.AddCard(new Card("A", Suit.SPADES, 11, 31));
+            _modelDeck.AddCard(new Card("7", Suit.DIAMONDS, 0, 0));
+            _modelDeck.AddCard(new Card("8", Suit.DIAMONDS, 0, 1));
+            _modelDeck.AddCard(new Card("9", Suit.DIAMONDS, 0, 2));
+            _modelDeck.AddCard(new Card("10", Suit.DIAMONDS, 10, 3));
+            _modelDeck.AddCard(new Card("J", Suit.DIAMONDS, 2, 4));
+            _modelDeck.AddCard(new Card("Q", Suit.DIAMONDS, 3, 5));
+            _modelDeck.AddCard(new Card("K", Suit.DIAMONDS, 4, 6));
+            _modelDeck.AddCard(new Card("A", Suit.DIAMONDS, 11, 7));
+            _modelDeck.AddCard(new Card("7", Suit.CLUBS, 0, 8));
+            _modelDeck.AddCard(new Card("8", Suit.CLUBS, 0, 9));
+            _modelDeck.AddCard(new Card("9", Suit.CLUBS, 0, 10));
+            _modelDeck.AddCard(new Card("10", Suit.CLUBS, 10, 11));
+            _modelDeck.AddCard(new Card("J", Suit.CLUBS, 2, 12));
+            _modelDeck.AddCard(new Card("Q", Suit.CLUBS, 3, 13));
+            _modelDeck.AddCard(new Card("K", Suit.CLUBS, 4, 14));
+            _modelDeck.AddCard(new Card("A", Suit.CLUBS, 11, 15));
+            _modelDeck.AddCard(new Card("7", Suit.HEARTS, 0, 16));
+            _modelDeck.AddCard(new Card("8", Suit.HEARTS, 0, 17));
+            _modelDeck.AddCard(new Card("9", Suit.HEARTS, 0, 18));
+            _modelDeck.AddCard(new Card("10", Suit.HEARTS, 10, 19));
+            _modelDeck.AddCard(new Card("J", Suit.HEARTS, 2, 20));
+            _modelDeck.AddCard(new Card("Q", Suit.HEARTS, 3, 21));
+            _modelDeck.AddCard(new Card("K", Suit.HEARTS, 4, 22));
+            _modelDeck.AddCard(new Card("A", Suit.HEARTS, 11, 23));
+            _modelDeck.AddCard(new Card("7", Suit.SPADES, 0, 24));
+            _modelDeck.AddCard(new Card("8", Suit.SPADES, 0, 25));
+            _modelDeck.AddCard(new Card("9", Suit.SPADES, 0, 26));
+            _modelDeck.AddCard(new Card("10", Suit.SPADES, 10, 27));
+            _modelDeck.AddCard(new Card("J", Suit.SPADES, 2, 28));
+            _modelDeck.AddCard(new Card("Q", Suit.SPADES, 3, 29));
+            _modelDeck.AddCard(new Card("K", Suit.SPADES, 4, 30));
+            _modelDeck.AddCard(new Card("A", Suit.SPADES, 11, 31));
         }
 
         private void ResetDeck()
         {
             _deck = new Deck(32);
-            for (var idx = 0; idx < modelDeck.Size(); idx += 1)
-                _deck.AddCard(modelDeck.GetDeck()[idx]);
+            for (var idx = 0; idx < _modelDeck.Size(); idx += 1)
+                _deck.AddCard(_modelDeck.GetDeck()[idx]);
         }
         
         private void DrawCards()
@@ -87,56 +87,56 @@ namespace Server
                     randCard = _deck.Size() > 1 ? rand.Next(1, _deck.Size()) : 0;
                     Console.Write("Giving card - ");
                     _deck.GetDeck()[randCard].PrintCard();
-                    Console.Write(" - to " + teams[idxTeam].GetPlayer(idxPlayer).GetName() +
-                                       " of " + teams[idxTeam].GetName() + "\n");
-                    teams[idxTeam].GetPlayer(idxPlayer).GetDeck().AddCard(_deck.GetDeck()[randCard]);
+                    Console.Write(" - to " + _teams[idxTeam].GetPlayer(idxPlayer).GetName() +
+                                       " of " + _teams[idxTeam].GetName() + "\n");
+                    _teams[idxTeam].GetPlayer(idxPlayer).GetDeck().AddCard(_deck.GetDeck()[randCard]);
                     _deck.RemoveCard(_deck.GetDeck()[randCard]);
                 }
                 idxPlayer += 1;
             }
         }
 
-        public void Broadcast(String message)
+        private void Broadcast(String message)
         {
             if (message.Equals("DECK"))
             {
-                teams[0].GetPlayer(0).SendDeck();
-                teams[0].GetPlayer(1).SendDeck();
-                teams[1].GetPlayer(0).SendDeck();
-                teams[1].GetPlayer(1).SendDeck();
+                _teams[0].GetPlayer(0).SendDeck();
+                _teams[0].GetPlayer(1).SendDeck();
+                _teams[1].GetPlayer(0).SendDeck();
+                _teams[1].GetPlayer(1).SendDeck();
                 return;
             }
-            teams[0].GetPlayer(0).SendMessage(message);
-            teams[0].GetPlayer(1).SendMessage(message);
-            teams[1].GetPlayer(0).SendMessage(message);
-            teams[1].GetPlayer(1).SendMessage(message);
+            _teams[0].GetPlayer(0).SendMessage(message);
+            _teams[0].GetPlayer(1).SendMessage(message);
+            _teams[1].GetPlayer(0).SendMessage(message);
+            _teams[1].GetPlayer(1).SendMessage(message);
         }
 
         private void PreBidding() {
-            teams[0].GetPlayer(0).SendMessage("MSG You are " + teams[0].GetPlayer(0).GetName() + ". Together with your mate "
-                                              + teams[0].GetPlayer(1).GetName() + ", you make " + teams[0].GetName() + ".");
-            teams[0].GetPlayer(1).SendMessage("MSG You are " + teams[0].GetPlayer(1).GetName() + ". Together with your mate "
-                                              + teams[0].GetPlayer(0).GetName() + ", you make " + teams[0].GetName() + ".");
-            teams[1].GetPlayer(0).SendMessage("MSG You are " + teams[1].GetPlayer(0).GetName() + ". Together with your mate "
-                                              + teams[1].GetPlayer(1).GetName() + ", you make " + teams[1].GetName() + ".");
-            teams[1].GetPlayer(1).SendMessage("MSG You are " + teams[1].GetPlayer(1).GetName() + ". Together with your mate "
-                                              + teams[1].GetPlayer(0).GetName() + ", you make " + teams[1].GetName() + ".");
+            _teams[0].GetPlayer(0).SendMessage("MSG You are " + _teams[0].GetPlayer(0).GetName() + ". Together with your mate "
+                                              + _teams[0].GetPlayer(1).GetName() + ", you make " + _teams[0].GetName() + ".");
+            _teams[0].GetPlayer(1).SendMessage("MSG You are " + _teams[0].GetPlayer(1).GetName() + ". Together with your mate "
+                                              + _teams[0].GetPlayer(0).GetName() + ", you make " + _teams[0].GetName() + ".");
+            _teams[1].GetPlayer(0).SendMessage("MSG You are " + _teams[1].GetPlayer(0).GetName() + ". Together with your mate "
+                                              + _teams[1].GetPlayer(1).GetName() + ", you make " + _teams[1].GetName() + ".");
+            _teams[1].GetPlayer(1).SendMessage("MSG You are " + _teams[1].GetPlayer(1).GetName() + ". Together with your mate "
+                                              + _teams[1].GetPlayer(0).GetName() + ", you make " + _teams[1].GetName() + ".");
         }
 
         private void AnnounceScores()
         {
-            Broadcast("MSG " + teams[0].GetName() + " (" + teams[0].GetPlayer(0).GetName() + ", "
-                      + teams[0].GetPlayer(1).GetName() + ") has " + teams[0].GetScore()
-                      + (teams[0].GetContract() == -1 ? "" : "/" + teams[0].GetContract()) + " points || "
-                      + teams[1].GetName() + " (" + teams[1].GetPlayer(0).GetName() + ", "
-                      + teams[1].GetPlayer(1).GetName() + ") has " + teams[1].GetScore()
-                      + (teams[1].GetContract() == -1 ? "" : "/" + teams[1].GetContract()) + " points. "
+            Broadcast("MSG " + _teams[0].GetName() + " (" + _teams[0].GetPlayer(0).GetName() + ", "
+                      + _teams[0].GetPlayer(1).GetName() + ") has " + _teams[0].GetScore()
+                      + (_teams[0].GetContract() == -1 ? "" : "/" + _teams[0].GetContract()) + " points || "
+                      + _teams[1].GetName() + " (" + _teams[1].GetPlayer(0).GetName() + ", "
+                      + _teams[1].GetPlayer(1).GetName() + ") has " + _teams[1].GetScore()
+                      + (_teams[1].GetContract() == -1 ? "" : "/" + _teams[1].GetContract()) + " points. "
                       + "Trump: " + _trump);
         }
 
         private int GetTrumpChooser()
         {
-            foreach (var team in teams)
+            foreach (var team in _teams)
             {
                 foreach (var player in team.GetPlayers())
                 {
@@ -147,31 +147,31 @@ namespace Server
             return (-1);
         }
 
-        public void CalculateScore()
+        private void CalculateScore()
         {
-            foreach (var team in teams)
+            foreach (var team in _teams)
             {
                 foreach (var player in team.GetPlayers())
                 {
-                    if (player.GetId() == trick.GetLeadingPlayer().GetId())
-                        team.AddScore(trick.GetValue());
+                    if (player.GetId() == _trick.GetLeadingPlayer().GetId())
+                        team.AddScore(_trick.GetValue());
                 }
             }
         }
 
-        public void AnnounceWinner()
+        private void AnnounceWinner()
         {
-            if (teams[0].HasWon(teams[1].GetScore()))
+            if (_teams[0].HasWon(_teams[1].GetScore()))
             {
-                Broadcast("MSG " + teams[0].GetName() + " has " + teams[0].GetScore() + " points and "
-                        + teams[1].GetName() + " has " + teams[1].GetScore() + ".");
-                Broadcast(teams[0].GetName() + "won! Congratulations!!");
+                Broadcast("MSG " + _teams[0].GetName() + " has " + _teams[0].GetScore() + " points and "
+                        + _teams[1].GetName() + " has " + _teams[1].GetScore() + ".");
+                Broadcast(_teams[0].GetName() + "won! Congratulations!!");
             }
             else
             {
-                Broadcast("MSG " + teams[0].GetName() + " has " + teams[0].GetScore() + " points and "
-                          + teams[1].GetName() + " has " + teams[1].GetScore() + ".");
-                Broadcast(teams[1].GetName() + "won! Congratulations!!");
+                Broadcast("MSG " + _teams[0].GetName() + " has " + _teams[0].GetScore() + " points and "
+                          + _teams[1].GetName() + " has " + _teams[1].GetScore() + ".");
+                Broadcast(_teams[1].GetName() + "won! Congratulations!!");
             }
         }
         
@@ -180,15 +180,15 @@ namespace Server
             var playerId = GetTrumpChooser();
             while (_allPlayers[0].GetDeck().Size() != 0)
             {
-                Broadcast("MSG " + teams[0].GetName() + " (" + teams[0].GetPlayer(0).GetName() + ", "
-                          + teams[0].GetPlayer(1).GetName() + ") has " + teams[0].GetScore()
-                          + (teams[0].GetContract() == -1 ? "" : "/" + teams[0].GetContract()) + " points || "
-                          + teams[1].GetName() + " (" + teams[1].GetPlayer(0).GetName() + ", "
-                          + teams[1].GetPlayer(1).GetName() + ") has " + teams[1].GetScore()
-                          + (teams[1].GetContract() == -1 ? "" : "/" + teams[1].GetContract()) + " points. "
+                Broadcast("MSG " + _teams[0].GetName() + " (" + _teams[0].GetPlayer(0).GetName() + ", "
+                          + _teams[0].GetPlayer(1).GetName() + ") has " + _teams[0].GetScore()
+                          + (_teams[0].GetContract() == -1 ? "" : "/" + _teams[0].GetContract()) + " points || "
+                          + _teams[1].GetName() + " (" + _teams[1].GetPlayer(0).GetName() + ", "
+                          + _teams[1].GetPlayer(1).GetName() + ") has " + _teams[1].GetScore()
+                          + (_teams[1].GetContract() == -1 ? "" : "/" + _teams[1].GetContract()) + " points. "
                           + "Trump: " + _trump);
                 AnnounceScores();
-                while (trick.Size() != 4)
+                while (_trick.Size() != 4)
                 {
                     _allPlayers[playerId].SendDeck();
                     _allPlayers[playerId].SendMessage("PLAY");
@@ -197,12 +197,12 @@ namespace Server
                     if (msg.Length != 2 && msg[0].Equals("PLAY") &&
                         Int32.TryParse(msg[1], out var cardId))
                     {
-                        if (_allPlayers[playerId].PutCard(trick, cardId, _trump))
+                        if (_allPlayers[playerId].PutCard(_trick, cardId, _trump))
                         {
                             Broadcast("MSG " + _allPlayers[playerId].GetName() + " put a "
-                                      + modelDeck.GetDeck()[cardId].GetName() + " of " +
-                                      modelDeck.GetDeck()[cardId].GetSuit()
-                                      + ". " + trick.GetLeadingPlayer().GetName() + " is leading this turn.");
+                                      + _modelDeck.GetDeck()[cardId].GetName() + " of " +
+                                      _modelDeck.GetDeck()[cardId].GetSuit()
+                                      + ". " + _trick.GetLeadingPlayer().GetName() + " is leading this turn.");
                             _allPlayers[playerId].SendMessage("PLAY OK");
                             playerId += 1;
                             playerId = (playerId >= 4 ? 0 : playerId);
@@ -212,9 +212,9 @@ namespace Server
                     }
                     else
                         _allPlayers[playerId].SendMessage("PLAY KO");
-                    playerId = trick.GetLeadingPlayer().GetId();
+                    playerId = _trick.GetLeadingPlayer().GetId();
                     CalculateScore();
-                    trick.ResetDeck();
+                    _trick.ResetDeck();
                 }
                 AnnounceWinner();
                 Broadcast("END");
@@ -249,8 +249,8 @@ namespace Server
                         continue;
                     }
                     var suit = msgTab[3];
-                    if (contract <= teams[0].GetContract()
-                            || contract <= teams[1].GetContract()
+                    if (contract <= _teams[0].GetContract()
+                            || contract <= _teams[1].GetContract()
                             || contract < 80
                             || contract % 10 != 0)
                     {
@@ -266,9 +266,9 @@ namespace Server
                         continue;
                     }
                     _trump = (Suit)Enum.Parse(typeof(Suit), suit.ToUpper());
-                    teams[(idPlayer == 0 || idPlayer == 2) ? 0 : 1].SetContract(contract);
-                    teams[(idPlayer == 0 || idPlayer == 2) ? 1 : 0].SetContract(-1);
-                    Broadcast("MSG " + player.GetName() + " from " + teams[(idPlayer == 0 || idPlayer == 2) ? 0 : 1].GetName()
+                    _teams[(idPlayer == 0 || idPlayer == 2) ? 0 : 1].SetContract(contract);
+                    _teams[(idPlayer == 0 || idPlayer == 2) ? 1 : 0].SetContract(-1);
+                    Broadcast("MSG " + player.GetName() + " from " + _teams[(idPlayer == 0 || idPlayer == 2) ? 0 : 1].GetName()
                     + " bid " + contract + " on " + suit.ToUpper());
                     if (!RecursiveBidding(idPlayer + 1, 3))
                         _allPlayers[idPlayer].SetTrumpChooser(true);
@@ -299,7 +299,7 @@ namespace Server
                     Broadcast("BID RESET");
             }
             Broadcast("BID STOP");
-            foreach (var team in teams)
+            foreach (var team in _teams)
             {
                 foreach (var player in team.GetPlayers())
                 {
