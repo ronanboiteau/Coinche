496c975536d561f4f3b5290b68a764f313c61477
Ronan Boiteau <ronan.boiteau@epitech.eu>
2017-11-12 23:09:32 +0100

    Preventing clients to connect when the game has started

--- Server/GameServer.cs
+++ Server/GameServer.cs
@@ -7,45 +7,42 @@ namespace Server
 {
     static class GameServer
     {
-        private static TcpListener Listener { get; set; }
-        private static bool Accept { get; set; }
-        private static int ConnectedClients { get; set; }
-        private static List<Player> _players = new List<Player>();
+        private static TcpListener _listener;
+        private static int _connectedClients;
+        private static readonly List<Player> Players = new List<Player>();
 
         public static void StartServer(string ip, int port)
         {
             var address = IPAddress.Parse(ip);
-            Listener = new TcpListener(address, port);
-            Listener.Start();
-            Accept = true;
+            _listener = new TcpListener(address, port);
+            _listener.Start();
             Console.Write($"Server up. Listening to TCP clients at {ip}:{port}\n");
         }
 
         public static void Listen()
         {
-            if (!(Listener != null && Accept))
-                return ;
-            while (ConnectedClients < 4)
+            while (_connectedClients < 4)
             {
-                Console.Write($"Waiting for more players... {ConnectedClients} connected at the moment.\n");
+                Console.Write($"Waiting for more players... {_connectedClients} connected at the moment.\n"); 
                 _listen();
             }
-            var team1 = new Team(_players[0], _players[2], "Team1");
-            var team2 = new Team(_players[1], _players[3], "Team2");
-            var game = new Game(_players, team1, team2);
+            _listener.Stop();
+            var team1 = new Team(Players[0], Players[2], "Team1");
+            var team2 = new Team(Players[1], Players[3], "Team2");
+            var game = new Game(Players, team1, team2);
             game.StartGame();
         }
 
         private static void _listen()
         {
-            var task = Listener.AcceptTcpClientAsync();
+            var task = _listener.AcceptTcpClientAsync();
             var client = task.Result;
             if (client == null)
                 return ;
             Console.Write("A new client just joined!\n");
-            ConnectedClients++;
-            var player = new Player(ConnectedClients - 1, "Player" + ConnectedClients, client);
-            _players.Add(player);
+            _connectedClients++;
+            var player = new Player(_connectedClients - 1, "Player" + _connectedClients, client);
+            Players.Add(player);
         }
     }
 }
